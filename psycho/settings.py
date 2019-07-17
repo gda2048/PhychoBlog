0831b89e2e257849dab99b3cf36e25ad5f0a66cc
gda2048 <goncharovdma@gmail.com>
2019-07-17 16:13:09 +0300

    fix whitenoise

--- psycho/settings.py
+++ psycho/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
