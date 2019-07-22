b45d9932111278ed22319a98a033b3530244edc6
gda2048 <goncharovdma@gmail.com>
2019-07-22 10:31:41 +0300

    add bootstrap4 using pip

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # PhychoBlog
+
+## heroku
+```
+heroku git:remote psychoblog
+heroku pg:backups:capture --app psychoblog
+
+heroku pg:backups:schedule DATABASE_URL --at '02:00 America/Los_Angeles' --app psychoblog
+
+```
--- events/urls.py
+++ events/urls.py
@@ -4,6 +4,6 @@ from .views import AnnouncementListView, EventListView, EventDetailView
 
 urlpatterns = [
                 path('announcement/', AnnouncementListView.as_view(), name='announcement_list'),
-                re_path('^events/(?P<type>[a-zA-Z0-9-]+)/$', EventListView.as_view(), name='event_list'),
+                re_path('^(?P<type>[a-zA-Z0-9-]+)/$', EventListView.as_view(), name='event_list'),
                 path(r'event/<int:pk>/', EventDetailView.as_view(), name='event'),
               ]
