203435b44dbbcff611830602482f9283acdebce3
gda2048 <goncharovdma@gmail.com>
2019-07-26 15:50:45 +0300

    fix articles + raw binaryfield

--- main/models.py
+++ main/models.py
@@ -2,6 +2,8 @@
 Stores main models of the project
 """
 from django.db import models
+from PIL import Image
+from io import BytesIO
 
 
 class PhotoItem(models.Model):
@@ -14,10 +16,22 @@ class PhotoItem(models.Model):
     alt = models.TextField("Описание фото", max_length=200, blank=True, null=True)
     height = models.PositiveIntegerField(null=True, blank=True)
     width = models.PositiveIntegerField(null=True, blank=True)
+    binary_image = models.BinaryField(null=True)
+    ext = models.CharField(max_length=10, null=True, blank=True)
 
     def save(self, *args, **kwargs):
-        if self.alt == '':
-            self.alt = self.photo.name
+        if self.photo:
+            self.photo.open()
+            with BytesIO() as output:
+                with Image.open(self.photo) as img:
+                    self.ext = img.format
+                    img.save(output, self.ext)
+                self.binary_image = output.getvalue()
+
+            if self.alt == '':
+                self.alt = self.photo.name
+        else:
+            self.binary_image = None
         super(PhotoItem, self).save(*args, **kwargs)
 
     class Meta:
