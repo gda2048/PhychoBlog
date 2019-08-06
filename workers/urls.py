48692184a1047770ff0bff44147a062f2c975cda
gda2048 <goncharovdma@gmail.com>
2019-08-06 10:15:20 +0300

    fix help_items list

--- workers/urls.py
+++ workers/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('author/', cache_page(60*15)(PersonListView.as_view()), name='person_list'),
     path('achievement/', AchievementListView.as_view(), name='achievement_list'),
     path(r'achievement/<int:pk>', cache_page(60*15)(AchievementDetailView.as_view()), name='achievement'),
-    path('help_item/', HelpItemListView.as_view(), name='help_item_list'),
+    path('help_item/', cache_page(60*15)(HelpItemListView.as_view()), name='help_item_list'),
     path(r'person/<int:pk>', cache_page(60*15)(PersonDetailView.as_view()), name='person'),
 ]
--- workers/views.py
+++ workers/views.py
@@ -31,9 +31,20 @@ class AchievementListView(ListView):
 
 
 class HelpItemListView(ListView):
-    model = HelpItem
+    queryset = Person.objects.prefetch_related("help_items").defer('email', 'birth_date', 'binary_image', 'info', 'bio')
     template_name = 'workers/help_items.html'
     context_object_name = 'help_item_list'
+
+    def get_context_data(self, **kwargs):
+        context = super(HelpItemListView, self).get_context_data(**kwargs)
+        last_articles = Article.objects.order_by('-release_date')[:2] \
+            .select_related('author').only("name", "id", "release_date", "author__id", "author__full_name", "content") \
+            .prefetch_related(Prefetch("photos", to_attr="ph", queryset=ArticlePhotoReport.objects.filter(main=True)
+                                       .only('photo', 'alt', 'id', 'height', "width", "article").distinct("article"))
+                              )
+        lst = list(next(iter(obj.ph), None) for obj in last_articles)
+        context['last_articles'] = list(zip(lst, list(last_articles)))
+        return context
 
 
 class PersonDetailView(DetailView):
