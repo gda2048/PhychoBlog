67f05717021c0d39ee2e622fb11dde1dafacbc93
gda2048 <goncharovdma@gmail.com>
2019-08-10 20:35:18 +0300

    add static_url while debug

--- psycho/urls.py
+++ psycho/urls.py
@@ -35,7 +35,8 @@ urlpatterns = [
                   path('shop/', include('shop.urls')),
                   path('admin/', admin.site.urls),
                   path('', include('events.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
+              + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 if settings.DEBUG:
