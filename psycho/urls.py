a5389a0eb62c5663bb499c676c21b800e04844b1
gda2048 <goncharovdma@gmail.com>
2019-08-17 00:22:44 +0300

    fix psycho

--- psycho/urls.py
+++ psycho/urls.py
@@ -1,18 +1,5 @@
-"""psycho URL Configuration
+import os
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -23,6 +10,9 @@ from events.models import Event
 from shop.models import Product
 from workers.models import Achievement, Person
 from .settings import site_name
+
+if not os.path.exists(settings.MEDIA_ROOT):
+    os.makedirs(settings.MEDIA_ROOT)
 
 admin.site.site_header = site_name
 admin.site.site_title = site_name
@@ -43,15 +33,8 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
-
-        # For django versions before 2.0:
-        # url(r'^__debug__/', include(debug_toolbar.urls)),
-
     ] + urlpatterns
 
-import os
-if not os.path.exists(settings.MEDIA_ROOT):
-    os.makedirs(settings.MEDIA_ROOT)
 ArticlePhotoReport.all_img_from_binary()
 Achievement.all_img_from_binary()
 Event.all_img_from_binary()
--- psycho/wsgi.py
+++ psycho/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for psycho project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,5 +5,4 @@ from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'psycho.settings')
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(get_wsgi_application())
